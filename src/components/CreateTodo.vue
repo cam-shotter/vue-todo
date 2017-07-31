3f6ed876cbc7cabc8d201b72979a75f9e9bba15b
Cameron Shotter <Cameron.Shotter@equinox.co.nz>
2017-08-01 11:10:26 +1200

    created CreateNew component

---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Create New</p>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,18 +3,21 @@
     <p class="tasks">Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p class="tasks">Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
     <todo v-for="todo in todos" :todo.sync="todo"></todo>
+    <create-todo></create-todo>
   </div>
 </template>
 
 <script type="text/javascript">
   import Todo from './Todo'
+  import CreateTodo from './CreateTodo'
 
   export default {
     props: [
       'todos'
     ],
     components: {
-      Todo
+      Todo,
+      CreateTodo
     }
   }
 </script>
