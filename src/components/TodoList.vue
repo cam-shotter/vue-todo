8a639bdb5e975eb248531eee4f18219281671c0a
cam-shotter <cameronshotter@gmail.com>
2017-08-01 16:52:11 +1200

    missed a revertComplete reference

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
   <div>
     <p class="tasks">Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p class="tasks">Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <todo v-on:delete-todo="deleteTodo" v-on:toggle-task-complete="toggleTaskComplete" v-on:revert-todo="revertTodo" v-for="todo in todos" :todo.sync="todo"></todo>
+    <todo v-on:delete-todo="deleteTodo" v-on:toggle-task-complete="toggleTaskComplete" v-for="todo in todos" :todo.sync="todo"></todo>
   </div>
 </template>
 
@@ -38,7 +38,7 @@ export default {
         sweetalert('Success!', 'Task completed!', 'success')
       } else {
         this.todos[todoIndex].done = false
-        sweetalert('Oops!', 'Better luck this time', 'success')
+        sweetalert('Oops, task not completed!', 'Better luck next time', 'success')
       }
     }
   }
