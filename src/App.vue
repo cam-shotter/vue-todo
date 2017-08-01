e3e719ce8a401956f039dd6f1cdeaaa2554984e0
cam-shotter <cameronshotter@gmail.com>
2017-08-01 14:56:44 +1200

    can add, edit and remove new todos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <div>
-    <todo-list v-bind:todos="todos"></todo-list>
+  <div id="app">
+    <h1 class="ui dividing centered header">Vue.js Todo App</h1>
+    <div class='ui three column centered grid'>
+      <div class='column'>
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import sweetalert from 'sweetalert'
 import TodoList from './components/TodoList'
-
+import CreateTodo from './components/CreateTodo'
 export default {
   name: 'app',
   components: {
-    TodoList
+    TodoList,
+    CreateTodo
   },
   data () {
     return {
@@ -32,17 +40,12 @@ export default {
         done: false
       }]
     }
+  },
+  methods: {
+    createTodo (newTodo) {
+      this.todos.push(newTodo)
+      sweetalert('Success!', 'To-Do created!', 'success')
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,52 +1,62 @@
 <template>
-  <div class="ui centered card">
+  <div class='ui centered card'>
     <div class="content" v-show="!isEditing">
-      <div class="header">
-        {{todo.title}}
+      <div class='header'>
+          {{ todo.title }}
       </div>
-      <div class="meta">
-        {{todo.project}}
+      <div class='meta'>
+          {{ todo.project }}
       </div>
-      <div class="extra content">
-        <span class="right floated edit icon" v-on:click="showForm"><i class="edit icon"></i></span>
-        <span class="right floated trash icon"><i class="trash icon"></i></span>
+      <div class='extra content'>
+          <span class='right floated edit icon' v-on:click="showForm">
+          <i class='edit icon'></i>
+        </span>
+        <span class='right floated trash icon' v-on:click="deleteTodo(todo)">
+          <i class='trash icon'></i>
+        </span>
       </div>
     </div>
     <div class="content" v-show="isEditing">
-      <div class="ui form">
-        <div class="field">
+      <div class='ui form'>
+        <div class='field'>
           <label>Title</label>
-          <input type="text" v-model="todo.title"/>
+          <input type='text' v-model="todo.title" >
         </div>
-        <div class="field">
+        <div class='field'>
           <label>Project</label>
-          <input type="text" v-model="todo.project"/>
+          <input type='text' v-model="todo.project" >
         </div>
-        <div class="ui two button attached buttons">
-          <button class="ui basic blue button" v-on:click="hideForm">Close X</button>
+        <div class='ui two button attached buttons'>
+          <button class='ui basic blue button' v-on:click="hideForm">
+            Close X
+          </button>
         </div>
       </div>
     </div>
-    <div class="ui bottom attached green basic button" v-show="!isEditing && todo.done">
-      Completed
+    <div class='ui bottom attached green basic button' v-show="!isEditing &&todo.done" disabled>
+        Completed
     </div>
-    <div class="ui bottom attached red basic button" v-show="!isEditing && !todo.done">
-      Pending
+    <div class='ui bottom attached red basic button' v-on:click="completeTodo(todo)" v-show="!isEditing && !todo.done">
+        Pending
     </div>
   </div>
 </template>
 
 <script type="text/javascript">
   export default {
-    props: [
-      'todo'
-    ],
+    props: ['todo'],
     data () {
       return {
         isEditing: false
       }
     },
     methods: {
+      completeTodo (todo) {
+        this.$emit('complete-todo', todo)
+      },
+      deleteTodo (todo) {
+        this.$emit('delete-todo', todo)
+      },
       showForm () {
         this.isEditing = true
       },
@@ -56,5 +66,3 @@
     }
   }
 </script>
-
-<style></style>
